8f7e7628094f382603a60084986c1c2825ed0d3b
Syed623-irfan <syedirfan6203@gmail.com>
2026-04-14 05:59:42 +0530

    script.js

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/submit", {
+            const response = await fetch("https://your-app-name.onrender.com/submit", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data)
